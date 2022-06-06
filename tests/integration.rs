67fd044f4d1ef49a36017e4d210d4ca4a90c02df
Marek Suchánek <msuchane@redhat.com>
2022-06-06 20:40:43 +0200

    Check standard fields in integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,8 +6,32 @@ use bugzilla_query::*;
 fn access_bug() {
     let _bug = bug(
         "https://bugzilla.redhat.com",
+        "1906883",
+        Authorization::Anonymous,
+    )
+    .unwrap();
+}
+
+/// Check that the bug fields contain the expected values.
+/// Work with fields that are standard in Bugzilla, rather than custom extensions.
+#[test]
+fn check_standard_fields() {
+    let bug = bug(
+        "https://bugzilla.redhat.com",
         "1906887",
         Authorization::Anonymous,
     )
     .unwrap();
+
+    assert_eq!(bug.id, 1906887);
+    assert_eq!(bug.summary, "Test the CoRN release notes generator (populated)");
+    assert_eq!(bug.status, "CLOSED");
+    assert_eq!(bug.resolution, "CURRENTRELEASE");
+    assert_eq!(bug.is_open, false);
+    assert_eq!(bug.component[0], "Documentation");
+    assert_eq!(bug.priority, "medium");
+    assert_eq!(bug.assigned_to, "Marek Suchánek");
+    assert_eq!(bug.assigned_to_detail.email, "msuchane");
+    assert_eq!(bug.docs_contact, "Marek Suchánek");
+    assert_eq!(bug.docs_contact_detail.unwrap().email, "msuchane");
 }
